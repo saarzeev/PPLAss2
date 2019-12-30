dd89ed4f0f19b059a08489917dcf05a668fb884d
saarzeev <saarzeev@post.bgu.ac.il>
2019-12-30 19:02:14 +0200

    Loading DB only once. Checking a starting point actually exists.

--- mybackend.py
+++ mybackend.py
@@ -6,8 +6,10 @@ import pandas as pd
 class MyBackend:
     def __init__(self):
         self._conn = sqlite3.connect("database.db")
+        if not self.doesTableExists():
+            self._initialize_db("BikeShare.csv")
 
-    def initialize_db(self, path):
+    def _initialize_db(self, path):
         self._create_table_in_db()
         self.load_csv_into_db(path)
         self._conn.commit()
@@ -16,6 +18,21 @@ class MyBackend:
     def _create_table_in_db(self):
         self._conn.execute(
             "CREATE TABLE IF NOT EXISTS bikeShare (TripDuration INTEGER ,StartTime TIMESTAMP ,StopTime TIMESTAMP, StartStationID INTEGER, StartStationName TEXT, StartStationLatitude REAL, StartStationLongitude REAL, EndStationID INTEGER, EndStationName TEXT, EndStationLatitude REAL, EndStationLongitude REAL, BikeID INTEGER, UserType TEXT, BirthYear INTEGER, Gender INTEGER, TripDurationinmin INTEGER)")
+
+    def doesTableExists(self):
+        tablename = 'bikeShare'
+        dbcur = self._conn.cursor()
+        dbcur.execute("""
+            SELECT COUNT(*)
+            FROM sqlite_master
+            WHERE type='table' AND name = '{0}'
+            """.format(tablename.replace('\'', '\'\'')))
+        if dbcur.fetchone()[0] == 1:
+            dbcur.close()
+            return True
+
+        dbcur.close()
+        return False
 
     def load_csv_into_db(self, path):
         with open(path, 'r') as f:
@@ -28,18 +45,17 @@ class MyBackend:
                 cursor.execute(query, data)
 
     def get_recommendations(self, starting_location, time, num_of_recommendations):
-        df = self._load_data_from_db(starting_location, time)
+        df = self._load_data_from_db()
         df_recommendations = self._create_recommendations(df, time, starting_location, num_of_recommendations)
         if df_recommendations is None:
             return []
         recommendations = df_recommendations.index.get_level_values('EndStationName').tolist()
         return recommendations
 
-    def _load_data_from_db(self, starting_location, time):
-        delta_time_percentage = 0.1
+    def _load_data_from_db(self):
 
-        #Select all trips that have a duration within +-delta_time_percentage of the requested time.
-        query = "SELECT * FROM bikeShare where StartStationName = '%s'" %(starting_location)
+        #Load all trips to memory
+        query = "SELECT StartStationName, StartStationLatitude, StartStationLongitude, TripDurationinmin, EndStationName FROM bikeShare"
 
         cursor = self._conn.execute(query)
 
@@ -49,28 +65,32 @@ class MyBackend:
         return df
 
     def _create_recommendations(self, df, time, starting_location, num_of_recommendations):
-        start_location_df = df[df['StartStationName'] == starting_location]
 
-        if len(start_location_df) == 0:
+        df = self._filter_distance_parameter(df, starting_location)
+        if df is None: #If no such starting_location exists in DB
             return None
-
-        df = self._filter_distance_parameter(df, start_location_df)
-        df = self._score_trips(df, time)
+        df = self._score_trips(df, time) #Add a score column to df
         df.sort_values(by=['score'], ascending=False, inplace=True)
 
         df = df.head(num_of_recommendations)
         return df
 
-    def _filter_distance_parameter(self, df, start_location_df):
-        x = start_location_df.iloc()[0]['StartStationLatitude']  # Get the requested starting point's latitude
-        y = start_location_df.iloc()[0]['StartStationLongitude']  # Get the requested starting point's longitude
-        # Calculate distance between requested point and other points
-        df['dist'] = np.sqrt(pow(df['StartStationLatitude'] - x, 2) + pow(df['StartStationLongitude'] - y, 2))
-        # print(df.shape)
-        # Keep only points within +-1KM
-        df = df.drop(df[df['dist'] > 0.001].index)
-        # print(df.shape)
-        return df
+    def _filter_distance_parameter(self, df, starting_location):
+        start_location_df = df[df['StartStationName'] == starting_location]
+
+        #If such starting point exists in DB
+        if starting_location in df['StartStationName'].values:
+
+            x = start_location_df.iloc()[0]['StartStationLatitude']  # Get the requested starting point's latitude
+            y = start_location_df.iloc()[0]['StartStationLongitude']  # Get the requested starting point's longitude
+            # Calculate distance between requested point and other points
+            df['dist'] = np.sqrt(pow(df['StartStationLatitude'] - x, 2) + pow(df['StartStationLongitude'] - y, 2))
+            # Keep only points within +-1KM
+            df = df.drop(df[df['dist'] > 0.001].index)
+
+            return df
+        else:
+            return None
 
     def _score_trips(self, df, requested_time):
         time_difference_weight = 1
@@ -79,7 +99,3 @@ class MyBackend:
         df['score'] = -(df['dist'] * distance_weight) - (np.abs(df['TripDurationinmin'] - requested_time) * time_difference_weight)
         return df
 
-
-nu = MyBackend()
-#nu.initialize_db("BikeShare.csv")
-print(nu.get_recommendations('5 Corners Library',4, 5))
